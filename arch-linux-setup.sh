9d1b4e05adc8de28ff1b487e262508702d3102b0
SUSANTA CHATTOPADHYAY <susantac@r1mbp2020-15-001.lan>
2022-01-17 17:17:54 -0500

    Adding setup for Arch Linux vm

---
+++ arch-linux-setup.sh
@@ -0,0 +1,11 @@
+
+# Arch Linux setup following two links:
+## https://wiki.archlinux.org/title/installation_guide
+## https://youtu.be/chApqIF0jRQ   
+
+# if the following ls works w/o error, i.e. EFI boot mode
+# ls /sys/firmware/efi/efivars 
+
+# Try to find out if internet is alredy setup
+# ip link
+# ping -c 2 www.google.com
